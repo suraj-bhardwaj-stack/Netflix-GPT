3b595fe6c4b2fa120d8b172e4bbee1ac5efb5790
suraj-bhardwaj-stack <surajbhardwaj000007@gmail.com>
2026-03-14 00:56:22 +0530

    create custom hook for nowPlayingMovies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,24 +1,9 @@
 import Header from "./Header";
 import theBg from '../assets/Netflix-bg.jpg'
-import { useEffect } from "react";
-import { API_OPTIONS , THE_MOVIE_API } from "../utils/constant";
-import { useDispatch } from "react-redux";
-import { callNowPlayingMovie } from "../utils/MovieSlice";
+import useNowPlayingMovies from "../Hooks/useNowPlayingMovies";
 
 const Browse = () =>{
-
-    const dispatch = useDispatch()
-
-    const fetchMovieNow = async ()=>{
-        const data =  await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${THE_MOVIE_API}`, API_OPTIONS)
-        const json = await data.json()
-        console.log(json.results)
-        dispatch(callNowPlayingMovie(json.results))
-    }
-    useEffect(()=>{
-        fetchMovieNow()
-    }, [])
-
+     useNowPlayingMovies()
     return(
         <div className="browse-wraper">
             <Header/>
